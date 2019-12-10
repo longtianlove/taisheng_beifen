ecd9973adad0754f551536b02baaf405b5a3e49a
龙 <bingodongtian@gmail.com>
2019-12-10 19:55:42 +0800

    轨迹地图

--- app/src/main/java/com/taisheng/now/map/TrackInstance.java
+++ app/src/main/java/com/taisheng/now/map/TrackInstance.java
@@ -16,8 +16,10 @@ import com.baidu.trace.model.ProcessOption;
 import com.baidu.trace.model.SortType;
 import com.baidu.trace.model.StatusCodes;
 import com.baidu.trace.model.TransportMode;
+import com.taisheng.now.bussiness.watch.WatchInstance;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class TrackInstance {
@@ -30,13 +32,18 @@ public class TrackInstance {
 
     LBSTraceClient mTraceClient;
     HistoryTrackRequest historyTrackRequest;
+
     public void init(Context context) {
         // 请求标识
         int tag = 1;
         // 轨迹服务ID
-        long serviceId = 218094;
+        long serviceId = 217993;
+
+        String deviceId = WatchInstance.getInstance().deviceId;
+        String deviceNumber = deviceId.substring(1, 3) + deviceId.substring(4, 6) + deviceId.substring(7, 10) + deviceId.substring(11, 14);
         // 设备标识
-        String entityName = "myTrace";
+        String entityName = deviceNumber;
+//        String entityName="9613050381";
         // 初始化轨迹服务
         Trace mTrace = new Trace(serviceId, entityName, false);
 // 初始化轨迹服务客户端
@@ -47,18 +54,7 @@ public class TrackInstance {
         historyTrackRequest = new HistoryTrackRequest(tag, serviceId, entityName);
 
         //设置轨迹查询起止时间
-// 开始时间(单位：秒)
-        long startTime = System.currentTimeMillis() / 1000 - 12 * 60 * 60;
-        // 结束时间(单位：秒)
-        long endTime = System.currentTimeMillis() / 1000;
-// 设置开始时间
-        historyTrackRequest.setStartTime(startTime);
-// 设置结束时间
-        historyTrackRequest.setEndTime(endTime);
 
-
-        // 设置需要纠偏
-        historyTrackRequest.setProcessed(true);
 
 // 创建纠偏选项实例
         ProcessOption processOption = new ProcessOption();
@@ -80,48 +76,62 @@ public class TrackInstance {
         historyTrackRequest.setSupplementMode(SupplementMode.driving);
 
 
-
-
-
     }
 
 
-   public void queryHistoryTrack(Context context, BaiduMap mBaiduMap){
-       // 初始化轨迹监听器
-       OnTrackListener mTrackListener = new OnTrackListener() {
-           // 历史轨迹回调
-           @Override
-           public void onHistoryTrackCallback(HistoryTrackResponse response) {
-               ArrayList trackPoints=new ArrayList();
+    public void queryHistoryTrack(Context context, BaiduMap mBaiduMap) {
+        Date startDate = new Date(2019, 12, 4);
 
-               int total = response.getTotal();
-               if (StatusCodes.SUCCESS != response.getStatus()) {
-                   Toast.makeText(context, "结果为：" + response.getMessage(), Toast.LENGTH_SHORT).show();
-               } else if (0 == total) {
-                   Toast.makeText(context, "未查询到历史轨迹", Toast.LENGTH_SHORT).show();
-               } else {
-                   List<TrackPoint> points = response.getTrackPoints();
-                   if (null != points) {
-                       for (TrackPoint trackPoint : points) {
-                           if (!TraceUtil.isZeroPoint(trackPoint.getLocation().getLatitude(),
-                                   trackPoint.getLocation().getLongitude())) {
-                               trackPoints.add(TraceUtil.convertTrace2Map(trackPoint.getLocation()));
-                           }
-                       }
-                   }
-               }
-               TraceUtil traceUtil = new TraceUtil();
-               traceUtil.drawHistoryTrack(mBaiduMap, trackPoints, SortType.asc);
+        // 开始时间(单位：秒)
+        long startTime = System.currentTimeMillis() / 1000 - 12 * 60 * 60;
+//       long startTime=1575388800;
+        // 结束时间(单位：秒)
+        long endTime = System.currentTimeMillis() / 1000;
+//       long endTime = 1575475199;
+
+// 设置开始时间
+        historyTrackRequest.setStartTime(startTime);
+// 设置结束时间
+        historyTrackRequest.setEndTime(endTime);
 
 
-           }
-       };
+        // 设置需要纠偏
+        historyTrackRequest.setProcessed(true);
+        // 初始化轨迹监听器
+        OnTrackListener mTrackListener = new OnTrackListener() {
+            // 历史轨迹回调
+            @Override
+            public void onHistoryTrackCallback(HistoryTrackResponse response) {
+                ArrayList trackPoints = new ArrayList();
+
+                int total = response.getTotal();
+                if (StatusCodes.SUCCESS != response.getStatus()) {
+                    Toast.makeText(context, "结果为：" + response.getMessage(), Toast.LENGTH_SHORT).show();
+                } else if (0 == total) {
+                    Toast.makeText(context, "未查询到历史轨迹", Toast.LENGTH_SHORT).show();
+                } else {
+                    List<TrackPoint> points = response.getTrackPoints();
+                    if (null != points) {
+                        for (TrackPoint trackPoint : points) {
+                            if (!TraceUtil.isZeroPoint(trackPoint.getLocation().getLatitude(),
+                                    trackPoint.getLocation().getLongitude())) {
+                                trackPoints.add(TraceUtil.convertTrace2Map(trackPoint.getLocation()));
+                            }
+                        }
+                    }
+                }
+                TraceUtil traceUtil = new TraceUtil();
+                traceUtil.drawHistoryTrack(mBaiduMap, trackPoints, SortType.asc);
+
+
+            }
+        };
 
 
 // 查询历史轨迹
-       mTraceClient.queryHistoryTrack(historyTrackRequest, mTrackListener);
+        mTraceClient.queryHistoryTrack(historyTrackRequest, mTrackListener);
 
-   }
+    }
 
 
 }
