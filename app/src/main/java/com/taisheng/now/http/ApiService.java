91280f605fcd13f85f7f8d51cccfc57dd081a416
龙 <bingodongtian@gmail.com>
2020-04-08 18:56:57 +0800

    上传语音接口参数修改

--- app/src/main/java/com/taisheng/now/http/ApiService.java
+++ app/src/main/java/com/taisheng/now/http/ApiService.java
@@ -149,9 +149,13 @@ import java.util.ArrayList;
 import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by long
@@ -563,6 +567,8 @@ public interface ApiService {
     @Multipart
     @POST(Constants.Url.Watch.microcharVoice)
     Call<BaseBean<PictureBean>> microcharVoice(
+            @Query("userId") String userId,
+            @Query("deviceId") String deviceId,
             @Part MultipartBody.Part file
     );
 
